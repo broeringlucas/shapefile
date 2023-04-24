d3593d69b9def7160177f45155317e3a71faf8da
broeringlucas <lucas10santos15@gmail.com>
2023-04-24 18:14:34 -0300

    commit10

--- app.js
+++ app.js
@@ -1,10 +1,10 @@
 const express = require('express')
 const app = express()
-const posts = require('./routes/postRoutes')
+const shapefileReader = require('./routes/shapefileRoute');
 
 app.use(express.json());
 
-app.use('/posts', posts);
+app.use('/shapefile', shapefileReader);
 
 app.use((err, req, res, next) => {
 	console.log(err.stack);
--- controllers/shapefileController.js
+++ controllers/shapefileController.js
@@ -4,54 +4,61 @@ const wkt = require('wkt');
 const decompress = require("decompress");
 
 
-const readShapefile = async function() {
-  // Aonde está snv_dividido.zip vai entrar o folder que o adm vai enviar 
-  // Esse else vai lançar uma exceção caso não seja possível ler o arquivo postado, caso não tenha um file.shp nele
-  const unzipResult = await decompress("snv_dividido.zip", "snv_dividido"); 
-  let filePath
-  for(let i = 0; i <= unzipResult.length - 1; i++)
-      if (unzipResult[i].path === 'icm.shp') {
-          filePath = `./snv_dividido/${unzipResult[i].path}`
-      } else {
-          console.log('erro')
-      }
+exports.readShapefile = async (req, res) => {
+  try {
+    const file = req.file.path
+    const unzipResult = await decompress(`${file}`, "snv_dividido"); 
+    let filePath
+    for(let i = 0; i <= unzipResult.length - 1; i++)
+        if (unzipResult[i].path === 'icm.shp') {
+            filePath = `./snv_dividido/${unzipResult[i].path}`
+        }
 
-  shp.open(filePath)
-    .then(source => source.read()
-      .then(function insertToTable(result) {
-        if (result.done) return;
-          //Converter geometry para tipo wkt
-          type_wkt = wkt.stringify(result.value.geometry)
-          let sql = `
-            INSERT INTO TB_RODOVIAS_TESTE (
-              ID,
-              CODE,
-              br,
-              uf,
-              km,
-              km_inicial,
-              km_final,
-              extensao,
-              superficie,
-              geom
-            )
-            VALUES(
-              '${result.value.properties.ID}',
-              '${result.value.properties.CODE}',
-              '${result.value.properties.HIGHWAY}',
-              '${result.value.properties.STATE}',
-              '${result.value.properties.KM}',
-              '${result.value.properties.KM_INI}',
-              '${result.value.properties.KM_FIN}',
-              '${result.value.properties.KM_FIN - result.value.properties.KM_INI}',
-              '${result.value.properties.SURFACE}',
-              ST_GeomFromText('${type_wkt}')
-            )`;
+    shp.open(filePath)
+      .then(source => source.read()
+        .then(function insertToTable(result) {
+          if (result.done) return;
+            //Converter geometry para tipo wkt
+            type_wkt = wkt.stringify(result.value.geometry)
+            let sql = `
+              INSERT INTO TB_RODOVIAS_TESTE (
+                ID,
+                CODE,
+                br,
+                uf,
+                km,
+                km_inicial,
+                km_final,
+                extensao,
+                superficie,
+                geom
+              )
+              VALUES(
+                '${result.value.properties.ID}',
+                '${result.value.properties.CODE}',
+                '${result.value.properties.HIGHWAY}',
+                '${result.value.properties.STATE}',
+                '${result.value.properties.KM}',
+                '${result.value.properties.KM_INI}',
+                '${result.value.properties.KM_FIN}',
+                '${result.value.properties.KM_FIN - result.value.properties.KM_INI}',
+                '${result.value.properties.SURFACE}',
+                ST_GeomFromText('${type_wkt}')
+              )`;
 
-          db.query(sql);
-          return source.read().then(insertToTable);
-      }))
-    .catch(error => console.error(error.stack));
-};
+            db.query(sql);
+            return source.read().then(insertToTable);
+        }))
+      .then(() => {
+        res.status(200).send('Registros inseridos com sucesso.');
+        })
+      .catch(error => {
+        console.error(error.stack);
+        res.status(500).send("Ocorreu um erro ao inserir os registros.");
+      });
 
-readShapefile();
+  } catch(error) {
+    console.log(error)
+    res.status(500).send("Ocorreu um erro ao ler o shapefile.")
+  }
+}
